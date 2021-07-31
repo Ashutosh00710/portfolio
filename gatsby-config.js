d3b402f8b50b666c624faa66238693aa6a44194a
Ashutosh00710 <starcdwivedi@gmail.com>
2021-07-31 23:52:57 +0530

    changed url

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,12 @@
 const config = require('./src/config');
 
 module.exports = {
+  pathPrefix: '/portfolio',
   siteMetadata: {
     title: 'Ashutosh Dwivedi',
     description:
       'Ashutosh Dwivedi is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    siteUrl: 'https://ashutosh00710.github.io/protfolio', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@AshutoshD07',
   },
